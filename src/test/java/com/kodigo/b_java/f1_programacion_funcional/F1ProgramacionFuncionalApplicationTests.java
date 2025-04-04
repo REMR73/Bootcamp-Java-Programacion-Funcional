5fb2d88185f0f708e4a7ce18fe6f7ad32a5d9a68
REMR11 <ronaldmejia0211@gmail.com>
2025-04-04 15:05:28 -0600

    Feat:Proyecto base

---
+++ src/test/java/com/kodigo/b_java/f1_programacion_funcional/F1ProgramacionFuncionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodigo.b_java.f1_programacion_funcional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1ProgramacionFuncionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
