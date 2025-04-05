7e2c5d07de3ec2cb593080abb9d3dbf1609d86d3
REMR11 <ronaldmejia0211@gmail.com>
2025-04-05 15:24:03 -0600

    Feat:Adapta proyecto spring para ejecutarse desde consola

--- src/main/java/com/kodigo/b_java/f1_programacion_funcional/F1ProgramacionFuncionalApplication.java
+++ src/main/java/com/kodigo/b_java/f1_programacion_funcional/F1ProgramacionFuncionalApplication.java
@@ -1,13 +1,20 @@
 package com.kodigo.b_java.f1_programacion_funcional;
 
+import com.kodigo.b_java.f1_programacion_funcional.principal.F1ProgramacionFuncional;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class F1ProgramacionFuncionalApplication {
+public class F1ProgramacionFuncionalApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(F1ProgramacionFuncionalApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		F1ProgramacionFuncional f1 = new F1ProgramacionFuncional();
+		f1.main();
+	}
 }
